da28234d59f64acea73578154d2a22396c004280
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-05-06 19:41:20 -0300

    initial commit

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,93 @@
+/* eslint-disable react/static-property-placement */
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Proptypes from 'prop-types';
+
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList } from './styles';
+// import { Container } from './styles';
+
+import api from '../../services/api';
+
+export default class Repository extends Component {
+    static propTypes = {
+        match: Proptypes.shape({
+            params: Proptypes.shape({
+                repository: Proptypes.string,
+            }),
+        }).isRequired,
+    };
+
+    // eslint-disable-next-line react/state-in-constructor
+    state = {
+        repository: {},
+        issues: [],
+        loading: true,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5,
+                },
+            }),
+        ]);
+
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false,
+        });
+    }
+
+    render() {
+        const { repository, issues, loading } = this.state;
+
+        if (loading) {
+            return <Loading> Carregando...</Loading>;
+        }
+        return (
+            <Container>
+                <Owner>
+                    <Link to="/">... Voltar aos repositórios </Link>
+                    <img
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
+                    />
+                    <h1>{repository.name}</h1>
+                    <p>{repository.description}</p>
+                </Owner>
+
+                <IssueList>
+                    {issues.map((issue) => (
+                        <li key={String(issue.id)}>
+                            <img
+                                src={issue.user.avatar_url}
+                                alt={issue.user.login}
+                            />
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url}>{issue.title}</a>
+                                    {issue.labels.map((label) => (
+                                        <span key={String(label.id)}>
+                                            {label.name}
+                                        </span>
+                                    ))}
+                                </strong>
+                                <p>{issue.user.login}</p>
+                            </div>
+                        </li>
+                    ))}
+                </IssueList>
+            </Container>
+        );
+    }
+}
